54b94235fced0ea4dd9f88432fc998c578f16a44
MobileChapter <104743430+MobileChapter@users.noreply.github.com>
2026-07-21 11:32:52 +1200

    Add files via upload

---
+++ ASBInterviewExercise/ASBInterviewExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASBInterviewExerciseApp.swift
+//  ASBInterviewExercise
+//
+//  Created by ASB on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ASBInterviewExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASBInterviewExerciseTests/ASBInterviewExerciseTests.swift
@@ -0,0 +1,19 @@
+//
+//  ASBInterviewExerciseTests.swift
+//  ASBInterviewExerciseTests
+//
+//  Created by ASB on 17/07/2026.
+//
+
+import Testing
+@testable import ASBInterviewExercise
+
+struct ASBInterviewExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
